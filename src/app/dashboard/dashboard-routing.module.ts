e37ae7baeeffc61a3bf44f88de32ecc66741360b
Gabriel Bertocco Biancardi <g.bbiancardi@gmail.com>
2020-01-28 10:28:43 -0300

    Profile page done.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { GraficoComponent } from './grafico/grafico.component';
 import { ObjetivoComponent } from './objetivo/objetivo.component';
 import { AtualizaComponent } from './atualiza/atualiza.component';
 import { DashboardComponent } from './dashboard.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { SuporteComponent } from './suporte/suporte.component';
 
 const routes: Routes = [
     {
@@ -32,6 +35,20 @@ const routes: Routes = [
                 component: AtualizaComponent,
                 data: {
                     title: 'Moneyger - Receita e Despesa'
+                }
+            },
+            {
+                path: 'perfil',
+                component: PerfilComponent,
+                data: {
+                    title: 'Moneyger - Perfil de Usuário'
+                }
+            },
+            {
+                path: 'suporte',
+                component: SuporteComponent,
+                data: {
+                    title: 'Moneyger - Suporte'
                 }
             }
         ]
